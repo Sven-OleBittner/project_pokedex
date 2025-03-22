c1cf46a9bb8688ae790851d31b82a21d7d8b2c0b
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-03-22 16:04:08 +0100

    add background type color and type circles

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
 const URL_DATA = "https://pokeapi.co/api/v2/";
+let limit = 20;
+let offset = 0;
 
 function init() {
   getPokemons();
 }
 
 async function getPokemons() {
-  const response = await fetch(`${URL_DATA}pokemon`);
+  const response = await fetch(
+    `${URL_DATA}pokemon?limit=${limit}&offset=${offset}`
+  );
   const data = await response.json();
-  let pokeKeys = Object.keys(data.results);
-  renderPokemons(data, pokeKeys);
+  renderPokemons(data);
 }
 
-async function renderPokemons(data, pokeKeys) {
-    let pokemonCardsRef = document.getElementById("pokemonCards");
-    pokemonCardsRef.innerHTML = "";
+async function renderPokemons(data) {
+  let pokeKeys = Object.keys(data.results);
+  let pokemonCardsRef = document.getElementById("pokemonCards");
+  pokemonCardsRef.innerHTML = "";
+
+  for (let i = 0; i < pokeKeys.length; i++) {
+    let types = await getPokemonType(i);
+    pokemonCardsRef.innerHTML += getPokemonTemplate(i, data, types);
+    renderTypesCircle(i, types);
+  }
+}
+
+async function renderTypesCircle(i, types) {
+  let typeCircleRef = document.getElementById(`${i+1}cardBody`);
+  let typeCircle = await getTypeCircle(types);
+  typeCircleRef.innerHTML = typeCircle;
   
-    for (let i = 0; i < pokeKeys.length; i++) {
-        let types = [];
-        const pokemonResponse = await fetch(`${URL_DATA}pokemon/${i + 1}`);
-        const pokemonData = await pokemonResponse.json();
-        pokemonData.types.forEach((type) => {
-          types.push(type.type.name);
-        });
-        pokemonCardsRef.innerHTML += getPokemonTemplate(i, data, types);
-      }
-    }
+}
+
+async function getPokemonType(i) {
+  let types = [];
+  const pokemonResponse = await fetch(`${URL_DATA}pokemon/${i + 1}`);
+  const pokemonData = await pokemonResponse.json();
+  pokemonData.types.forEach((type) => {
+    types.push(type.type.name);
+  });
+  return types;
+}
+
+async function getTypeCircle(types) {
+  if (types.length == 1) {
+    return getTypeCircleSingle(types);
+  } else {
+    return getTypeCircleDouble(types);
+  }
+  
+}
+
+async function loadMore() {
+  if (limit != 1302) {
+    limit += 20;
+    // offset += 20;
+    const response = await fetch(
+      `${URL_DATA}pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await response.json();
+    renderPokemons(data);
+  }
+}
--- scripts/template.js
+++ scripts/template.js
@@ -1,18 +1,25 @@
 function getPokemonTemplate(i, data, types) {
   return `
-   <div class="card" style="width: 12rem">
+   <div class="card " style="width: 12rem">
                   <h4 class="card-title">
                     <span class="poke_name"> #${i+1} ${data.results[i].name} </span>
                   </h4>
                   <img
                     src="./sprites/sprites/pokemon/${i + 1}.png"
-                    class="card-img-top"
+                    class="card-img-top ${types[0]}"
                     alt="${data.results[i].name}"
                   />
-                  <div class="card-body">
-                    <p class="card-text">
-                    types: ${types.join(", ")}
-                    </p>
+                  <div id="${i+1}cardBody" class="card-body d-flex font-weight-bold justify-content-center">
+                    <p class="card-text">                    
                   </div>
                 </div>`;
+}
+
+function getTypeCircleSingle(params) {
+  return `<p class="circle ${params}">${params}</p>`;
+}
+
+function getTypeCircleDouble(params) {
+  return `<p class="circle ${params[0]}">${params[0]}</p>
+          <p class="circle ${params[1]}">${params[1]}</p>`;
 }
