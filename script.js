c910b650be947fc1bfea4eaf50393d1668025a75
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-05-24 15:16:47 +0200

    Get the APT Data and Renderring the Pokemons

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const URL_DATA = "https://pokeapi.co/api/v2/";
+const pokeList = [];
+let limit = 20;
+
 function init() {
-    
+  getPokeDatas();
+}
+
+async function getPokeDatas() {
+  let response = await fetch(URL_DATA + `pokemon?limit=${limit}&offset=0`);
+  let pokeData = await response.json();
+  renderPokemons(pokeData);
+}
+
+function showLoadingSpinner() {
+  document.getElementById("lodingSpinner").innerHTML =
+    "<img src='./assets/img/pokeball-loader.svg'>";
+}
+
+async function renderPokemons(pokeData) {
+    showLoadingSpinner();
+  let pokeContainer = document.getElementById("pokeList");
+  pokeContainer.innerHTML = "";
+  for (let pokeIndex = 0; pokeIndex < pokeData.results.length; pokeIndex++) {
+    let url = pokeData.results[pokeIndex].url;
+    let response = await fetch(url);
+    let data = await response.json();
+    let id = data.id;
+    let name = getPokeName(data);
+    let sprite = data.sprites.front_default;
+    let types = getPokeTypes(data);
+    let bg = data.types[0].type.name;
+    pokeContainer.innerHTML += getPokemonTemplate(id, name, sprite, types, bg);
+  }
+}
+
+function getPokeName(data) {
+  let lowerCaseName = data.name;
+  let upperCaseName =
+    lowerCaseName.charAt(0).toUpperCase() + lowerCaseName.slice(1);
+  return upperCaseName;
+}
+
+function getPokeTypes(data) {
+  let types = data.types
+    .map(
+      (type) =>
+        `<img src="./assets/types/${type.type.name}.png" alt="${type.type.name}" class="type-icon">`
+    )
+    .join(" ");
+    return types;
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,3 +1,11 @@
-function getPokemonTemplate(pokeIndex) {
-    return ``;
+function getPokemonTemplate(id, name, sprite, types, bg) {
+    return `<div
+        id="pokemonContainer"
+        class="pokemon_container site_padding top_bottom_padding ${bg} type_container"
+      >
+        <span>ID : ${id}</span>
+        <span>Name : ${name}</span>
+        <img src="${sprite}" alt="Bisa" />
+        <span class="types_container">${types}</span>
+      </div>`;
 }
