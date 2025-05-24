7756d86945e1640a1313af2c7c7afb93cbe042eb
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-05-24 16:17:44 +0200

    Change id and name color

--- scripts/template.js
+++ scripts/template.js
@@ -3,8 +3,8 @@ function getPokemonTemplate(id, name, sprite, types, bg) {
         id="pokemonContainer"
         class="pokemon_container site_padding top_bottom_padding ${bg} type_container"
       >
-        <span>ID : ${id}</span>
-        <span>Name : ${name}</span>
+        <span class="poke_id">#${id}</span>
+        <span class="poke_name">${name}</span>
         <img class="poke_sprite" src="${sprite}" alt="Bisa" />
         <span class="types_container">${types}</span>
       </div>`;
